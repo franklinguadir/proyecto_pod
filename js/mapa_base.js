82f4eb02ade984b22220d03d300ae424b043fc45
franklinguadir <franklin.guadir@correounivalle.edu.co>
2021-05-01 16:19:53 -0500

    v1_01-05-2021

--- js/mapa_base.js
+++ js/mapa_base.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([1.2146013, -77.2782755], 8);
+var mymap = L.map('mapid').setView([1.39322, -77.6497169], 8);
             L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
